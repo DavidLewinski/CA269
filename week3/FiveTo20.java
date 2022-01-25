ccbd3564a29fe9ab4513957ad57fe595b451805e
David Lewinski <davidlewinski02@gmail.com>
2022-01-25 12:46:45 +0000

    Adding final week3 work.

---
+++ week3/FiveTo20.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FiveTo20
+{
+    public static void main(String [] args)
+    {
+        for (int i = 5; i <= 20; i++)
+        {
+            System.out.print(i + " ");
+        }
+
+        System.out.println(); // Finish with a new line.
+    }
+}
---
+++ week3/TenTo1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TenTo1
+{
+    public static void main(String [] args)
+    {
+        for (int i = 10; i >= 1; i--)
+        {
+            System.out.print(i + " ");
+        }
+
+        System.out.println(); // Finish with a new line.
+    }
+}
